c2200b962f46ba6571910cef50f1ce011751a065
mlsnyder <mlsnyder@unknown>
2006-10-25 22:03:21 +0000

    impl parsing now works

--- LayerParser.hs
+++ LayerParser.hs
@@ -4,16 +4,18 @@ import Lang
 import UU.Parsing
 import UU.Scanner
 
-ops  = [":"]
-keys = ["layer","extends","params","uses","pattern","in","out","inout","interface"]
+lyrops  = [":"]
+lyrkeys = ["layer","extends","params","uses","pattern","in","out","inout","interface"]
 
 main :: IO ()
 main = do { layer <- parseLayer "Equation.inf"
           ; putStr $ show layer
+          ; impl <- parseImpl "Equation.impl"
+          ; putStr $ show impl
           }
 
 parseLayer :: String -> IO Layer
-parseLayer file = do { tokens <- scanFile	keys ops "{(:)}" "" file
+parseLayer file = do { tokens <- scanFile	lyrkeys lyrops "{(:)}" "" file
                      ; parseIO pLayer tokens
                      }
 
@@ -48,3 +50,44 @@ pDirection :: Parser Token Direction
 pDirection =    (In <$ pKey "in")
             <|> (InOut <$ pKey "inout")
             <|> (Out <$ pKey "out")
+
+{-------
+implementation of Equational
+  rule fitsIn implements Expr
+    pre
+    post R : Expr = "kiGam ; tyGam ; valGam :- int : tyInt"
+-------}
+
+implops  = [":","="]
+implkeys = ["implementation","of","rule","implements","pre","post"]
+
+parseImpl :: String -> IO Implementation
+parseImpl file = do { tokens <- scanFile implkeys implops "{(:)}=" "" file
+                    ; parseIO pImpl tokens
+                    }
+
+pImpl :: Parser Token Implementation
+pImpl = Implementation_RawImplementation 
+                  <$  pKey "implementation"
+                  <*  pKey "of"
+                  <*> pConid
+                  <*> pList pRule
+
+pRule :: Parser Token Rule
+pRule = Rule_RawRule <$  pKey "rule"
+                  <*> pVarid
+                  <*  pKey "implements"
+                  <*> pConid
+                  <*> opt (pKey "pre" *> pList pJudge) []
+                  <* pKey "post" <*> pList pJudge
+
+pJudge = Judgment_RawJudgment <$> pConid 
+                              <*  pKey ":"
+                              <*> pConid
+                              <*  pKey "="
+                              <*> pString
+                              <*> pSucceed []
+
+
+
+
