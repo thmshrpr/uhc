3dad0728ba9b650f76c708dde80dc19028a845f5
JohnVanSchie <JohnVanSchie@unknown>
2006-11-12 17:38:23 +0000

    Fixed Commandline interface

--- EHC/sandbox/lang/Main.hs
+++ EHC/sandbox/lang/Main.hs
@@ -8,16 +8,25 @@ import System.Environment
 
 main :: IO ()
 main = do { args <- getArgs
-          ; putStr (show args)
-          ; impls <- compile "Known"
-          ; putStr $ impls
+          ; res <- if null args
+                   then return errOut
+                   else do let fName = head args
+                           compile fName
+                           return $ succesMsg fName
+          ; putStr res
           }
 
-compile :: String -> IO (String)
+errOut :: String
+errOut = "The application shoud be passed at least one parameter, being the name of the implementation to be transformed. E.g. if your implementation is in 'Equational.impl', you should invoke the appliation with the parameter 'Equational'\n" 
+
+succesMsg :: String -> String
+succesMsg fName = "Succesfully wrote the transformed output to " ++ fName ++ ".hs\n"
+
+compile :: String -> IO ()
 compile name = do rawlayer <- parseLayer name
                   target   <- processLayer name rawlayer 
                   let layers = hierarchy target
                   impls  <- resolveImpls layers
                   let merged = merge (reverse impls)
-                  return $ show (sem_Implementation merged name)
+                  writeFile (name ++ ".hs") $ show (sem_Implementation merged name)
 
