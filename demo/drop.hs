8f5448a025e1152a4a7143b5e61a9d65f5c437a2
jmdjoski <jmdjoski@unknown>
2009-01-28 13:12:00 +0000

    Updated the drop-demo to produce output that makes sense

--- demo/drop.hs
+++ demo/drop.hs
@@ -2,11 +2,19 @@ data Bool = False | True
 data ''[]'' a = a : [a] | ''[]''
 
 foreign import ccall "primEqInt"  (==) :: Int -> Int -> Bool
+foreign import ccall "primAddInt" (+) :: Int -> Int -> Int
 foreign import ccall "primSubInt" (-) :: Int -> Int -> Int
+
+undefined = False
 
 drop :: Int -> [Int] -> [Int]
 drop n [] = []
-drop n (x:xs) = drop (n-1) xs
+drop n (x:xs) | n == 0 = xs
+              | True   = drop (n-1) (x:xs)
 
-main = drop 2 [1,2,3,4,5]
+length :: [Int] -> Int
+length [] = 0
+length (x:xs) = 1 + length xs
+
+main = length (drop 2 [1,2,3,4,5])
 
